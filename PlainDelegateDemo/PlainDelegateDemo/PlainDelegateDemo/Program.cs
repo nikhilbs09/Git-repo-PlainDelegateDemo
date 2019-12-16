e778db5d2f0e4045ae314949fb06921bea87e89e
NIKS SHINE <nikhilbs09@gmail.com>
2019-12-17 00:36:30 +0530

    Create Program.cs

---
+++ PlainDelegateDemo/PlainDelegateDemo/PlainDelegateDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlainDelegateDemo
+{
+    class Program
+    {
+        public delegate void StringPrinter(string s1);
+
+        static void Main(string[] args)
+        {
+            var printer = new StringPrinter(PrintString);
+
+            bool esqual = true;
+
+            bool uneqsual = false;
+
+            bool result = esqual & uneqsual;
+            if (result)
+                printer("Hello");
+            else
+                printer("bye-bye");
+
+            Console.ReadLine();
+            
+        }
+
+        static void PrintString(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
